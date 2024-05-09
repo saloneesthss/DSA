2e301d331886532a29b0d2f3d95b39532330feca
Salonee Shrestha <saloneeshrestha35@gmail.com>
2024-05-09 08:53:03 +0545

    Added insertion sort

---
+++ Sort/insertion_sort.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int arr[]={5,4,6,2,11};
+    int n=sizeof(arr)/sizeof(arr[0]);
+
+    printf("Unsorted array: ");
+    for (int i=0; i<n; i++) {
+        printf("%d",arr[i]);
+    }
+    //Main logic starts here
+    for (int i=1;i<n;i++) {
+        int current=arr[i];
+        int j=i-1;
+        while (j>=0 && current<arr[j]) {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=current;
+    }
+    //Main logic ends here
+
+    printf("\nSorted array: ");
+    for (int i=0;i<n;i++) {
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
